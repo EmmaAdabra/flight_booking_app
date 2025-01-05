34906a3cff1c25ebf71878f955cff93b95a455e7
EmmaAdabra <adabraemma@gmail.com>
2025-01-05 12:26:59 +0000

    Add StringUtility class
    contains methods for manipulating strings

---
+++ src/main/java/utils/LoggingUtil.java
@@ -0,0 +1,21 @@
+package utils;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.Appender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingUtil {
+    public static void disableConsoleAppender(){
+        // Get the LoggerContext from Logback
+//        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        // Cast the root logger to Logback's Logger class
+//        Logger rootLogger = loggerContext.getLogger(Logger.ROOT_LOGGER_NAME);
+
+        // Find the ConsoleAppender
+//        Appender consoleAppender = rootLogger.getAppender("CONSOLE");
+
+//        rootLogger.
+    }
+}
